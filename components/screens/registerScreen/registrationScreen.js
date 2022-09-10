f2a88535f5886100db992b4dd2e797367ee1f003
Tim Yang <tyang098@gmail.com>
2022-09-10 09:36:54 -0400

    adding button to sign up

--- components/screens/registerScreen/registrationScreen.js
+++ components/screens/registerScreen/registrationScreen.js
@@ -4,6 +4,10 @@ import React, {useState} from 'react';
 import Input from "../../Input";
 import { Button } from "@react-native-material/core";
 
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
 export default function RegistrationScreen(){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -12,11 +16,17 @@ export default function RegistrationScreen(){
   return(
 
     <View style={styles.root}>
+      <View style={{flexDirection:"row"}}>
+        <Text style={styles.title}>GasUp</Text>
+      </View>
+  
       <Text style={styles.title}>Create a Account</Text>
       <Input placeholder={"First Name"} value={username} setValue={setUsername}></Input>
       <Input placeholder={"Last Name"}></Input>
       <Input placeholder={"Password"} value={password} setValue={setPassword} secureTextEntry={"true"}></Input>
-      <Button variant="contained"color="black">Contained</Button>
+      <Button variant="contained"color="black" style={styles.button} title="SIGN UP">Contained</Button>
+      <Separator/>
+      <Text style={styles.text}>Already have an account? Click here to sign in.</Text>
     </View>
 
   )
@@ -31,9 +41,27 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   title: {
+    marginLeft: 15,
     marginTop: 200,
+    marginBottom: 10,
     fontWeight: 'bold',
     fontSize: 24,
     color: 'Black',
   },
+  button:{
+    marginLeft:12,
+    marginRight:12,
+    marginTop:20
+  },
+  separator: {
+    marginLeft: 9,
+    marginRight: 9,
+    marginTop: 15,
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  text:{
+    marginLeft: 12
+  }
 })
