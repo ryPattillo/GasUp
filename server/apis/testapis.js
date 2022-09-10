d573117760ff056e26925b2cb72c350a6044cb4b
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-09-10 02:30:02 -0400

    Added new test API endpoint

--- server/apis/testapis.js
+++ server/apis/testapis.js
@@ -11,5 +11,11 @@ module.exports = {
 
       res.status(200).json({info: "working2!"});
     });
+
+    app.get("/api/carInfo", async (req, res, next) => 
+    {
+      res.status(200).json({info: "Get Car Info here! "});
+    });
+
   },
 };
