106bae81ef8efb3aa4388ec5ef9729dc3721457b
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-09-10 03:01:14 -0400

    Added new test API endpoint

--- server/apis/testapis.js
+++ server/apis/testapis.js
@@ -19,7 +19,9 @@ module.exports = {
     app.get("/api/carInfo", async (req, res, next) => 
     {
       var data;
-      axios.get('https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2012&make=Honda&model=Fit').then((res)=>{data=res.data});
+      axios.get('https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2012&make=Honda&model=Fit', {headers: {
+        'Content-Type': 'application/xml'}
+    }).then((res)=>{data=res.data});
       res.status(200).json({info: data});
     });
 
