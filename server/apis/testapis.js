7eaf300dfa3f3e98400a9588c0dd9d38c92421db
Luis Mata <lui@Luiss-MacBook-Pro.local>
2022-09-10 01:12:03 -0400

    add test database connection

--- server/apis/testapis.js
+++ server/apis/testapis.js
@@ -4,6 +4,11 @@ module.exports = {
     // test APIs
     app.post("/api/test", async (req, res, next) => 
     {
+      admin.firestore()
+      .collection("users")
+      .doc(`${Math.floor(Math.random() * 10000000)}`)
+      .set({"username" : "fakeuser"});
+
       res.status(200).json({info: "working2!"});
     });
   },
