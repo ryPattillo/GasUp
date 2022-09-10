0b49357ed83923242bc9c3da17300a2e80130739
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-09-10 17:09:12 -0400

    Getting location to work

--- server/apis/routeapis.js
+++ server/apis/routeapis.js
@@ -7,39 +7,44 @@ module.exports = {
      * Endpoint for using MapBox matching API with a list of coordinates
      */
     app.post("/api/mapBox", async (req, res, next) => {
-      let coordinates = req.body;
-      coordinates = JSON.parse(coordinates);
-      // Get string list of coordinate for request
-      coordinates.map((element) => {
-        coordinate_string += element.lat + "," + element.long + ";";
-      });
-      // Ensure that last is not in request
-      coordinate_string = coordinate_string.substring(
-        0,
-        coordinate_string.length - 1
-      );
-      
-      try {
-        const resp = await axios.get(
-          `https://api.mapbox.com/matching/v5/mapbox/driving/${coordinate_string}?access_token=${process.env.mapbox_key}`,
-          {
-            headers: {
-              accept: "application/json",
-            },
-          }
+      if (req && req.body) {
+        let coordinates = req.body;
+        coordinates = JSON.parse(coordinates);
+        // Get string list of coordinate for request
+        coordinates.map((element) => {
+          coordinate_string += element.lat + "," + element.long + ";";
+        });
+        // Ensure that last is not in request
+        coordinate_string = coordinate_string.substring(
+          0,
+          coordinate_string.length - 1
         );
-        if (resp && resp.data) {
-          let map_data = resp.data;
-          console.log(
-            "Total Miles " + map_data["matchings"][0]["distance"] * 0.000621371
-          );
 
-          res.status(200).json({ response: "Success" });
-        } else {
-          res.status(400).json({ error: "Bad response from MapBox api" });
+        try {
+          const resp = await axios.get(
+            `https://api.mapbox.com/matching/v5/mapbox/driving/${coordinate_string}?access_token=${process.env.mapbox_key}`,
+            {
+              headers: {
+                accept: "application/json",
+              },
+            }
+          );
+          if (resp && resp.data) {
+            let map_data = resp.data;
+            console.log(
+              "Total Miles " +
+                map_data["matchings"][0]["distance"] * 0.000621371
+            );
+
+            res.status(200).json({ response: "Success" });
+          } else {
+            res.status(400).json({ error: "Bad response from MapBox api" });
+          }
+        } catch (error) {
+          res.status(400).json({ error: "Error with MapBox api " });
         }
-      } catch (error) {
-        res.status(400).json({ error: "Error with MapBox api " });
+      } else {
+        res.status(204).json({ error: "No Request Body given " });
       }
     });
   },
