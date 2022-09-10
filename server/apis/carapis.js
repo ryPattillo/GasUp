da9f0a227c436344364cc0b11f6cf53f7d07d7fb
ryPattillo <ryan_pattillo@knights.ucf.edu>
2022-09-10 12:19:32 -0400

    Updating before merge

--- server/apis/carapis.js
+++ server/apis/carapis.js
@@ -3,16 +3,6 @@ const axios = require("axios");
 module.exports = {
   // APIs
   apis: function (app, admin) {
-    // test APIs
-    app.post("/api/test", async (req, res, next) => {
-      admin
-        .firestore()
-        .collection("users")
-        .doc(`${Math.floor(Math.random() * 10000000)}`)
-        .set({ username: "fakeuser" });
-
-      res.status(200).json({ info: "working2!" });
-    });
 
     /**
      * Endpoint for getting a list of available trims of a make,model, and year
