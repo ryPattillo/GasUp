c68683b7159a4e62a0e84ecc87398ef07b2ab61b
Luis Mata <lui@Luiss-MacBook-Pro.local>
2022-09-10 12:59:08 -0400

    format login screen

--- components/screens/loginScreen/LoginScreen.js
+++ components/screens/loginScreen/LoginScreen.js
@@ -1,72 +1,96 @@
-
-import { View, Text, StyleSheet, Image, Alert, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform} from "react-native";
-import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Alert,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
 import Input from "../../Input";
 import { Button } from "@react-native-material/core";
 import { HStack } from "@react-native-material/core";
-import { useAuth } from '../../contexts/AuthContext';   
+import { useAuth } from "../../contexts/AuthContext";
 
-const Separator = () => (
-  <View style={styles.separator} />
-);
+const Separator = () => <View style={styles.separator} />;
 
-export default function LoginScreen(){
-
-
-
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+export default function LoginScreen() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const { signup, login } = useAuth();
 
   async function loginUser() {
     try {
-      if(email === ''){
+      if (email === "") {
         Alert.alert("Enter a email!");
-      }
-      else if(password === ''){
+      } else if (password === "") {
         Alert.alert("Enter a passoword!");
-      }
-      else{
-        const result = await login(email,password);
+      } else {
+        const result = await login(email, password);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
- 
-  return(
 
+  return (
     <KeyboardAvoidingView
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-    style={styles.container} >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View style={styles.root}>
-        <HStack mt={-75} ml={-20} spacing={0} style={{justifyContent: 'center', borderStyle: 'solid'}}>
-         <Text style={styles.logoText}>GasUp</Text>
-         <Image source={require('../../../assets/images/dragonLogo.png')} style={styles.logoImage}/>
-        </HStack>
-        
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.root}>
+          <HStack
+            mt={-75}
+            ml={-20}
+            spacing={0}
+            style={{ justifyContent: "center", borderStyle: "solid" }}
+          >
+            <Text style={styles.logoText}>GasUp</Text>
+            <Image
+              source={require("../../../assets/images/dragonLogo.png")}
+              style={styles.logoImage}
+            />
+          </HStack>
 
-     
-      <Text style={styles.title}>Create a Account</Text>
-      <Input placeholder={"Email"} value={email} setValue={setEmail}></Input>
-      <Input placeholder={"Password"} value={password} setValue={setPassword} secureTextEntry={"true"}></Input>
-      <Button variant="contained"color="black" style={styles.button} title="LOGIN" onPress={loginUser}></Button>
+          <Text style={styles.title}>Create a Account</Text>
+          <Input
+            placeholder={"Email"}
+            value={email}
+            setValue={setEmail}
+          ></Input>
+          <Input
+            placeholder={"Password"}
+            value={password}
+            setValue={setPassword}
+            secureTextEntry={"true"}
+          ></Input>
+          <Button
+            variant="contained"
+            color="black"
+            style={styles.button}
+            title="LOGIN"
+            onPress={loginUser}
+          ></Button>
 
-      <Button variant="contained"color="black" style={styles.button} title="SIGN UP" ></Button>
-      <Separator/>
-      <Text style={styles.text}>Forgot your password? Click Here.</Text>
-    </View>
-    </TouchableWithoutFeedback>
+          <Button
+            variant="contained"
+            color="black"
+            style={styles.button}
+            title="SIGN UP"
+          ></Button>
+          <Separator />
+          <Text style={styles.text}>Forgot your password? Click Here.</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
-
-  )
-
-  
+  );
 }
 
 const styles = StyleSheet.create({
-
   root: {
     // flex: 1,
     // justifyContent: 'center',
@@ -77,45 +101,42 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     marginTop: 55,
     marginBottom: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 30,
-    color: 'Black'
+    color: "Black",
   },
-  button:{
-    marginLeft:12,
-    marginRight:12,
-    marginTop:20
+  button: {
+    marginLeft: 12,
+    marginRight: 12,
+    marginTop: 20,
   },
   separator: {
     marginLeft: 9,
     marginRight: 9,
     marginTop: 15,
     marginVertical: 8,
-    borderBottomColor: '#737373',
+    borderBottomColor: "#737373",
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
-  text:{
-    marginLeft: 12
+  text: {
+    marginLeft: 12,
   },
-  logoText:{
+  logoText: {
     marginLeft: 15,
     marginTop: 200,
     marginBottom: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 60,
-    color: 'Black',
-
+    color: "Black",
   },
-  logoImage:{
-
+  logoImage: {
     marginLeft: 0,
     marginTop: 200,
     marginBottom: 10,
-    width: 60, 
+    width: 60,
     height: 60,
-
   },
   container: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
