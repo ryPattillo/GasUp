10b1a4ada8716b6a5f27fe5552e041ccaeebac6b
Luis Mata <lui@Luiss-MacBook-Pro.local>
2022-09-10 08:42:15 -0400

    format file

--- server/apis/testapis.js
+++ server/apis/testapis.js
@@ -1,33 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-
   // APIs
   apis: function (app, admin) {
     // test APIs
-    app.post("/api/test", async (req, res, next) => 
-    {
-      admin.firestore()
-      .collection("users")
-      .doc(`${Math.floor(Math.random() * 10000000)}`)
-      .set({"username" : "fakeuser"});
+    app.post("/api/test", async (req, res, next) => {
+      admin
+        .firestore()
+        .collection("users")
+        .doc(`${Math.floor(Math.random() * 10000000)}`)
+        .set({ username: "fakeuser" });
 
-      res.status(200).json({info: "working2!"});
+      res.status(200).json({ info: "working2!" });
     });
 
-    app.get("/api/carInfo", async (req, res, next) => 
-    {
+    app.get("/api/carInfo", async (req, res, next) => {
       var vehicle_id;
       try {
-        const resp = await axios.get('https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2012&make=Honda&model=Fit',
-        {headers: {
-          'accept': 'application/json'
-      },});
-        if (resp && resp.data)
-        {
+        const resp = await axios.get(
+          "https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2012&make=Honda&model=Fit",
+          {
+            headers: {
+              accept: "application/json",
+            },
+          }
+        );
+        if (resp && resp.data) {
           let vehicle_data = resp.data;
           console.log(vehicle_data);
-          
+
           // vehicle_data looked like:
           //
           // {
@@ -42,27 +43,19 @@ module.exports = {
           // do something with the vehicle
           //await axios.get ..
 
-          res.status(200).json({response: "should be here"});
+          res.status(200).json({ response: "should be here" });
+        } else {
+          res.status(400).json({ error: "Bad response from fueleconomy api" });
         }
-        else
-        {
-          res.status(400).json({error: "Bad response from fueleconomy api"});
-        }
-        
       } catch (error) {
-        res.status(400).json({error: "Error from fuel economy api"});
+        res.status(400).json({ error: "Error from fuel economy api" });
       }
-
 
       // axios.get().then((resp)=>{
       //   vehicle_id = resp.data["info"]["menuItem"][0]["value"];
-      // }); 
+      // });
 
       // axios.get('https://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/26425').then((resp)=>{
-        
-      
-
     });
-
   },
 };
